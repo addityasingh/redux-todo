0d6ea098863c5c24fa767c1dc56db7a0df14f1ac
Aditya Singh <adisinghrajput@gmail.com>
2016-04-08 12:08:20 +0530

    Add Todo component to TodoList

--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -1,11 +1,42 @@
 import React, { Component } from 'react';
 
+import Todo from './Todo';
+
 export default class TodoList extends Component {
+  componentDidMount () {
+    const { store } = this.context;
+    this.unsubscribe = store.subscribe(() => {
+      this.forceUpdate();
+    });
+  }
+
+  componentWillUnmount () {
+    this.unsubscribe();
+  }
+
   render() {
+    //TODO:Remove hardcoded todo list once add todo implemented
+    const initialTodos = [
+        { text: 'Aditya' },
+        { text: 'Pratap' },
+        { text: 'Singh' }
+      ];
+
+    const { store } = this.context;
+    const { todos, visibilityFilter } = store.getState();
+
     return (
-      <div>
-       Todo List demo
-      </div>
+      <ul>
+        {
+          initialTodos.map((t) => {
+           return  <Todo { ...t }/>
+          })
+        }
+      </ul>
     );
   }
+}
+
+TodoList.contextTypes = {
+  store: React.PropTypes.object
 }
