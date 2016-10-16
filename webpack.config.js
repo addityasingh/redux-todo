e9f5c1949d2f5b7f9b44336d5768959a37ca1d26
Aditya Pratap Singh <adisinghrajput@gmail.com>
2016-10-16 21:46:35 +0200

    Revert "Webpack bundle generation"
    This reverts commit e209f2a4628a0926d513aba1ca2e46d347980427.

--- webpack.config.js
+++ webpack.config.js
@@ -1,25 +1,20 @@
 var path = require('path');
 var webpack = require('webpack');
 
-var ENVIRONMENT = process.env.NODE_ENV || 'production';
-var IS_PROD = ENVIRONMENT === 'production';
-
 module.exports = {
-  devtool: IS_PROD ? 'cheap-module-source-map' : 'eval',
-  entry: './src/index.js',
+  devtool: 'eval',
+  entry: [
+    'webpack-dev-server/client?http://localhost:3000',
+    'webpack/hot/only-dev-server',
+    './src/index.js'
+  ],
   output: {
     path: path.join(__dirname, 'dist'),
-    filename: 'bundle.js'
+    filename: 'bundle.js',
+    publicPath: '/static/'
   },
   plugins: [
-    new webpack.HotModuleReplacementPlugin(),
-    IS_PROD ? 
-      new webpack.DefinePlugin({
-        'process.env': {
-        'NODE_ENV': JSON.stringify('production')
-        }
-    }) :
-    null
+    new webpack.HotModuleReplacementPlugin()
   ],
   module: {
     loaders: [{
